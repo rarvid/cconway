6ca3d22f7dc82134240e69bfe078e0f7298386c2
Rudolfs Arvids Kalnins <rudolfs@ricefield.dzintari>
2021-02-13 23:57:48 +0200

    added comments and removed trash

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Conway's game of life in C
+# Conway's game of life in C displayed via terminal
 takes input text file with specific Alive and Dead fields
 and displays output generations on terminal
--- conway.c
+++ conway.c
@@ -8,12 +8,13 @@
 #define INPUTD 'O'
 #define ALIVE '#'
 #define DEAD '.'
-// Conways game of life render 100x100 boarder but display
-// smaller for more accurate simulation
+// Conways game of life rendered on VIS_SIZE x VIS_SIZE
+// with a single row border all around with DEAD cells to negate border effects
 char screen[WHOLE_SIZE][WHOLE_SIZE];
 char newstates[WHOLE_SIZE][WHOLE_SIZE];
-int nofnei[WHOLE_SIZE][WHOLE_SIZE];
 
+// convert input simbols to preattier simbols for better viewing
+// if you want to input with different simbols change INPUTA and INPUTD for ALIVE and DEAD RESPECTIVELY
 int convert() {
   for(int i = 0; i < WHOLE_SIZE; i++) {
     for(int j = 0; j <WHOLE_SIZE; j++) {
@@ -26,6 +27,7 @@ int convert() {
   return 0;
 }
 
+// initialize 'screen' with DEAD cells 
 int initScreen(char arr[WHOLE_SIZE][WHOLE_SIZE]) {
   for(int i = 0; i < WHOLE_SIZE; i++) {
     for(int j = 0; j < WHOLE_SIZE; j++) {
@@ -35,6 +37,7 @@ int initScreen(char arr[WHOLE_SIZE][WHOLE_SIZE]) {
   return 0;
 }
 
+// read in input file and convert it to 2D matrix
 int readInitCond(FILE* f) {
   char temp;
   for(int i = 1; i < WHOLE_SIZE; i++) {
@@ -49,6 +52,7 @@ int readInitCond(FILE* f) {
   return 0;
 }
 
+// iterate over matrix and check each cells next generation state and save it seperate matrix
 int checkStates() {
   int neighbours = 0;
   for(int i = 1; i < WHOLE_SIZE - 1; i++) {
@@ -65,7 +69,7 @@ int checkStates() {
         }
       }
 
-      /* Rule actuator */
+      /* Rule actuator(The Rules Of The Game) */ 
       if (screen[i][j] == ALIVE) {
         if (neighbours < 2)
           newstates[i][j] = DEAD;
@@ -78,13 +82,13 @@ int checkStates() {
         if (neighbours == 3)
           newstates[i][j] = ALIVE;
       }
-      nofnei[i][j] = neighbours;
       neighbours = 0;
     }
   }
   return 0;
 }
 
+// copy next generation states to current screen
 int updateScreen() {
   for(int i = 1; i < WHOLE_SIZE - 1; i++) {
     for(int j = 1; j < WHOLE_SIZE - 1; j++) {
@@ -94,6 +98,7 @@ int updateScreen() {
   return 0;
 }
 
+// print 2D array in an ok way
 int printScreen() {
   for(int i = 1; i < WHOLE_SIZE - 1; i++) {
     for(int j = 1; j < WHOLE_SIZE - 1; j++) {
@@ -108,25 +113,20 @@ int main() {
   FILE* f;
   if ((f = fopen("input.txt", "r")) == NULL)
     exit(1);
-
+  
+  // fill current screen and next gen state matricies with dead cells
   initScreen(screen);
   initScreen(newstates);
-
+  
+  // read in input file
   readInitCond(f);
+  // convert input simbols to output simbols
   convert();
 
+  // print starting gen
   printScreen();
 
-//  checkStates();
-//  updateScreen();
-//printf("number of neighbours\n");
-//for(int i = 1; i < WHOLE_SIZE - 1; i++) {
-//  for(int j = 1; j < WHOLE_SIZE - 1; j++) {
-//    printf(" %d", nofnei[i][j]);
-//  }
-//  printf("\n");
-//}
-
+  // infinitely calculate next generation
   while(1) {
     checkStates();
     updateScreen();
@@ -134,7 +134,6 @@ int main() {
     printf("\033[0;0H");  // place cursor at top left also C escape char BS
     printScreen();
     fflush(stdout);
-    //printf("\033[?25h");  // restore terminal ????
     usleep(1000 * 100);  // sleep in microseconds
   }
 
